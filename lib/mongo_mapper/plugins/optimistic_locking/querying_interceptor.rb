681c3da5af025ee5601d9f8a2880a7a0c91ab088
Luis Correa d'Almeida <luis.ca@gmail.com>
2015-11-19 18:32:20 +0000

    fix for https://github.com/mongomapper/mongomapper/issues/550

--- lib/mongo_mapper/plugins/optimistic_locking/querying_interceptor.rb
+++ lib/mongo_mapper/plugins/optimistic_locking/querying_interceptor.rb
@@ -10,12 +10,17 @@ module MongoMapper
 
         def save_to_collection_with_optimistic_locking(options = {})
           if persisted? && keys.keys.include?("_lock_version")
+
+            # Delete this key from the options hash, otherwise we get hit by
+            # this: https://github.com/mongomapper/mongomapper/issues/550
+            options.delete(:persistence_method)
+
             current_lock_version = self._lock_version
             begin
               self._lock_version += 1
               result = collection.update({:_id => self._id, :$or => [ {:_lock_version => current_lock_version},  { :_lock_version => { :$exists => false } } ] },
                                           to_mongo, :upsert => false, :safe => true)
-                                          
+
               raise MongoMapper::StaleDocumentError.new(self) unless result["updatedExisting"]
             rescue
               self._lock_version -= 1
